57b883aa04782610f36bb2fc0a2d731a86da3676
osnlab <osn@tuta.io>
2020-05-16 17:31:45 +0300

    Initial commit

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode == 404">
+      Page not found
+    </h1>
+    <h1 v-else>
+      An error occured
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
